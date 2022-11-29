dc21536b7f5b211d93639ac71c6e9ab83aff3b93
Aygun <aygun.gasimova@inbox.ru>
2022-11-29 19:20:37 +0400

    Task 10 completed

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Insert 3 digit number");
+int number = int.Parse(Console.ReadLine());
+int n1 = number / 10;
+int n2 = n1 % 10;
+Console.WriteLine("Middle digit : " + n2);
