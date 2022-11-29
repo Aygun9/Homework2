5ef81f3888348f37d769f1bf27fd1031cf88b6b5
Aygun <aygun.gasimova@inbox.ru>
2022-11-29 20:43:00 +0400

    Task 13 completed

---
+++ Task13/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Insert number");
+int number = int.Parse(Console.ReadLine());
+if (number <= 99)
+{
+    Console.WriteLine("No 3rd digit");
+}
+if ((number >= 100) && (number <= 999))
+{
+    int n3 = number % 10;
+    Console.WriteLine($"3rd digit :  -> {n3}");
+}
+if (number > 999)
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+    {
+        Console.WriteLine($"3rd digit :  -> {number % 10}");
+    }
+}
