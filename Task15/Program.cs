34be8c2b27a970c28d753c99e81895413b826bf4
Aygun <aygun.gasimova@inbox.ru>
2022-11-29 21:21:59 +0400

    Task 15 completed

---
+++ Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write(" Please Insert week day number");
+int number = int.Parse(Console.ReadLine());
+if ((number >= 1) && (number <= 5))
+{
+    Console.WriteLine("No - weekday");
+}
+if ((number >= 6) && (number <= 7))
+{
+    Console.WriteLine("yes - weekend");
+}
+else
+{
+    Console.WriteLine("Incorrect number ");
+}
